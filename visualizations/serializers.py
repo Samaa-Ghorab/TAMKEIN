bd0dabb575bfba4601b1cf380f6ef853f28b5d16
MohamedGad <138883562+II-MohamedGad-II@users.noreply.github.com>
2025-06-28 20:22:36 +0300

    Delete visualizations directory

--- visualizations/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class VideoProcessingSerializer(serializers.Serializer):
-    video = serializers.FileField()
-    number_of_images = serializers.IntegerField(default=2)
